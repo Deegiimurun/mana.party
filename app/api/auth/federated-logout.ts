27ae0c4d8b57dccba2cd150566a9e79000fe4237
Nate Houk <nate@epsilonrecords.com>
2023-09-05 17:36:53 +0200

    Refactor

--- app/api/auth/federated-logout.ts
+++ app/api/auth/federated-logout.ts
@@ -1,16 +1,17 @@
 // /api/auth/federated-logout
-import jwt from "next-auth/jwt"
-import log from "utils/server-logger"
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getToken } from "next-auth/jwt"
 
-export default async function federatedLogout(req, res) {
+export default async function federatedLogout(req: NextApiRequest, res: NextApiResponse) {
   try {
-    const token = await jwt.getToken({req, secret: process.env.SECRET, encryption: true })
+    const token = await getToken({req, secret: process.env.SECRET, encryption: true })
     if (!token) {
-      log.warn("No JWT token found when calling /federated-logout endpoint")
-      return res.redirect(process.env.NEXTAUTH_URL)
+      if (process.env.NEXTAUTH_URL) {
+        return res.redirect(process.env.NEXTAUTH_URL)
+      } else {
+        return res.status(500);
+      }
     }
-    if (!token.idToken)
-      log.warn("Without an id_token the user won't be redirected back from the IdP after logout.")
 
     const endsessionURL = `https://${process.env.AUTH0_ISSUER}/oidc/logout`
     const endsessionParams = new URLSearchParams([
@@ -19,7 +20,10 @@ export default async function federatedLogout(req, res) {
     ]);
     return res.redirect(`${endsessionURL}?${endsessionParams}`)
   } catch (error) {
-    log.error(error)
-    res.redirect(process.env.NEXTAUTH_URL)
+    if (process.env.NEXTAUTH_URL) {
+      res.redirect(process.env.NEXTAUTH_URL)
+    } else {
+      return res.status(500);
+    }
   }
 }
