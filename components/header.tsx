5e7cf1cc7f77459b1eb4673b0d58afc67cb82df3
Nate Houk <nate@epsilonrecords.com>
2023-09-05 14:46:14 +0200

    Refactor

--- components/header.tsx
+++ components/header.tsx
@@ -47,7 +47,7 @@ export async function Header() {
             <UserMenu user={session.user} />
           ) : (
             <Button variant="link" asChild className="-ml-2">
-              <Link href="/sign-in?callbackUrl=/">Login</Link>
+              <Link href="/api/auth/login">Login</Link>
             </Button>
           )}
         </div>
